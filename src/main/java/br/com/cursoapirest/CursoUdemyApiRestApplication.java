5525bdd0b79a181ebeb5116e33221385a0af30f4
jardriel sousa <jardriel.redes18@gmail.com>
2020-04-21 17:25:46 -0300

    first commit

---
+++ src/main/java/br/com/cursoapirest/CursoUdemyApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.cursoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemyApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemyApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cursoapirest/CursoUdemyApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemyApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
